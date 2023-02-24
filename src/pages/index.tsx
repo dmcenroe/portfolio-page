22d8c9eea010c24e9be81d4a33d005d68aca7fad
Derek McEnroe <derek.mcenroe@gmail.com>
2023-02-24 10:54:37 -0600

    skills page responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,7 +106,7 @@ export default function Home() {
                     headSize={"3"}
                     startAnchor={"bottom"}
                     endAnchor={"right"}
-                  />{" "}
+                  />
                 </div>
               ) : null}
             </div>
